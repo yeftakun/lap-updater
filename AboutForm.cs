900afaff8938e8ea6a7eb433f3b1c1bfdab77cd8
yeftakun <yeftakun34@gmail.com>
2026-01-14 19:07:46 +0800

    about

--- AboutForm.cs
+++ AboutForm.cs
@@ -20,7 +20,21 @@ internal sealed class AboutForm : Form
         {
             AutoSize = true,
             MaximumSize = new Size(460, 0),
-            Text = "Lorem ipsum skibidi toilet...",
+            Text =
+                "Lap Time Updater" + Environment.NewLine +
+                Environment.NewLine +
+                "A small utility to quickly update your lap times to your website repo." + Environment.NewLine +
+                Environment.NewLine +
+                "Features:" + Environment.NewLine +
+                "- Copy personalbest.ini into the repo data folder" + Environment.NewLine +
+                "- Check changes and push via git" + Environment.NewLine +
+                "- Edit website config (src/data/config.json) via Preference" + Environment.NewLine +
+                "- Configure Side Image (image, width, based on picture)" + Environment.NewLine +
+                Environment.NewLine +
+                "App config location:" + Environment.NewLine +
+                "%APPDATA%\\LapUpdater\\config.json" + Environment.NewLine +
+                Environment.NewLine +
+                "Note: make sure git is available and the repo path is correct before pushing.",
             Padding = new Padding(12)
         };
 
